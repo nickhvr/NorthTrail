a7fd9f34b2f601c1bf34b5d6ff724cc28a4cda6f
nickhvr <nickhaverkamp@icloud.com>
2026-04-07 11:14:30 +0200

    fix

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,31 +1,26 @@
 import { getAllPosts, getPillarSummaries } from '@/lib/posts'
 import { siteConfig } from '@/lib/site'
 
+export const dynamic = 'force-static'
+
 export default function sitemap() {
   const baseUrl = siteConfig.url
+
   const posts = getAllPosts()
   const pillars = getPillarSummaries()
 
-  const postUrls = posts.map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.date ? new Date(post.date) : new Date(),
-  }))
-
-  const pillarUrls = pillars.map((pillar) => ({
-    url: `${baseUrl}/blog/${pillar.slug}`,
-    lastModified: new Date(),
-  }))
-
   return [
-    {
-      url: baseUrl,
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/blog`, lastModified: new Date() },
+
+    ...pillars.map((p) => ({
+      url: `${baseUrl}/blog/${p.slug}`,
       lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/blog`,
-      lastModified: new Date(),
-    },
-    ...pillarUrls,
-    ...postUrls,
+    })),
+
+    ...posts.map((post) => ({
+      url: `${baseUrl}/blog/${post.slug}`,
+      lastModified: new Date(post.date),
+    })),
   ]
 }
