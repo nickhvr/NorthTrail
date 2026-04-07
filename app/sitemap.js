65723f90d75a6f8aeebb822658ff7b3497c61fa2
nickhvr <nickhaverkamp@icloud.com>
2026-04-07 12:59:46 +0200

    fix: sitemap url pillar missing

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,23 +4,36 @@ import { siteConfig } from '@/lib/site'
 export const dynamic = 'force-static'
 
 export default function sitemap() {
-  const baseUrl = siteConfig.url
-
+  const baseUrl = siteConfig.url.replace(/\/$/, '')
   const posts = getAllPosts()
   const pillars = getPillarSummaries()
 
-  return [
-    { url: baseUrl, lastModified: new Date() },
-    { url: `${baseUrl}/blog`, lastModified: new Date() },
-
-    ...pillars.map((p) => ({
-      url: `${baseUrl}/blog/${p.slug}`,
+  const staticUrls = [
+    {
+      url: baseUrl,
       lastModified: new Date(),
-    })),
-
-    ...posts.map((post) => ({
-      url: `${baseUrl}/blog/${post.slug}`,
-      lastModified: new Date(post.date),
-    })),
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
   ]
+
+  const pillarUrls = pillars.map((pillar) => ({
+    url: `${baseUrl}/blog/${pillar.slug}`,
+    lastModified: new Date(),
+  }))
+
+  const postUrls = posts.map((post) => {
+    const postPath = Array.isArray(post.pathParts) && post.pathParts.length > 0
+      ? post.pathParts.join('/')
+      : `${post.pillar}/${post.slug}`
+
+    return {
+      url: `${baseUrl}/blog/${postPath}`,
+      lastModified: new Date(post.date),
+    }
+  })
+
+  return [...staticUrls, ...pillarUrls, ...postUrls]
 }
