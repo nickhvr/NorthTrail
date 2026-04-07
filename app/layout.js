717471af9611fd8deab5c22c54354f793fd27ce4
nickhvr <nickhaverkamp@icloud.com>
2026-04-07 10:53:22 +0200

    searchconsole: metatag for ownershipconfirmation

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
   icons: {
     icon: imageConfig.logo,
   },
+  verification: {
+    google: "THLoZw9uH6iFbSji5uqX4aIFQ9K1Qq2KvXi0lbzSJx8"
+  }
 };
 
 export default function RootLayout({ children }) {
